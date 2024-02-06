e172360ff73055626539ec411c55131d0fc96da8
romel <romel.lamp@tktk.ee>
2024-02-07 00:06:49 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes valikust, rakendus tervitab kasutajat järgmiselt:'
+//"Welcome Mr. [kasutaja perekonnanimi]" / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (M/F):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andemeid string (sõne) formaadis
+
+Console.WriteLine("Please, enter your last name:");
+
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Rakendus küsib kasutajal sisestada tema vanust
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+//"You are too young to use this App." muul juhul "Welcome to the App!"
+
+Console.WriteLine("Please enter your age, to use this App:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean - true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+//int userAge  = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old."); //userAgeNum");
+
+if(isAgeNumber)
+{
+    if(userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome, to the App!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use this App.");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
